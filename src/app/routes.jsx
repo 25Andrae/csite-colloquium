6975da1bba9327889b33829b6b3f14ffd1709d5d
Andrae <manguilimotanandrae25@gmail.com>
2026-03-10 22:00:31 +0800

    Com

--- src/app/routes.jsx
+++ src/app/routes.jsx
@@ -19,9 +19,10 @@ export const router = createBrowserRouter([
       { path: "organizing-committee", Component: OrganizingCommittee },
       { path: "evaluation", Component: EvaluationForm },
     ],
-  },
+  }
+],
   {
     // ADD THIS HERE: Your GitHub repository name
-    basename: "/csite-colloquium/",
+    basename: "/csite-colloquium",
   }
-]);
+);
